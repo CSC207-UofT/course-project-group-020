a093f72224651bdea925b2297d351ab4cedbb1ba
Yousuf Hassan <yousuf.hassan@mail.utoronto.ca>
2021-10-14 12:11:18 -0400

    Created a new branch to work on UI

--- src/main/java/CommandLine/UIMain.java
+++ src/main/java/CommandLine/UIMain.java
@@ -5,39 +5,5 @@ public class UIMain {
 
     public static void main(String[] args) {
 
-        Scanner username = null;
-        Scanner password = null;
-
-        username = new Scanner(System.in);
-        password = new Scanner(System.in);
-
-        UICreateAccount createUI = new UICreateAccount(username, password);
-        createUI.runCreateAccount();
-//
-        Scanner userInput = null;
-        Scanner userInputSub = null;
-        Scanner userExit = null;
-
-        userInput = new Scanner(System.in);
-        userInputSub = new Scanner(System.in);
-        userExit = new Scanner(System.in);
-
-
-        UIMenu menu = new UIMenu(userInput, userInputSub);
-        menu.run();
-
-        System.out.println("To Exit, type 'EXIT' now, or press enter to go back to main menu");
-        String userExitString = userExit.nextLine();
-        if (userExitString.equals("STOP")){
-            System.out.println("True");
-        }
-
-        userInput.close();
-        userInputSub.close();
-        userExit.close();
-
-
-        }
-
-        }
+    }
 
--- src/main/java/Manager.java
+++
@@ -1,6 +0,0 @@
-public class Manager {
-    //this is just an initial push
-    public static void main(String[] args) {
-        System.out.println("print line");
-    }
-}
