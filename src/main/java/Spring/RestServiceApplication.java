ca183dee40079952bf03cdfb3582b6f339bdacc6
Ryan Zhao <ryansiyuan.zhao@mail.utoronto.ca>
2021-11-09 20:01:35 -0500

    test

--- src/main/java/Spring/RestServiceApplication.java
+++ src/main/java/Spring/RestServiceApplication.java
@@ -1,2 +1,14 @@
-package Spring;public class RestServiceApplication {
+package Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
 }
