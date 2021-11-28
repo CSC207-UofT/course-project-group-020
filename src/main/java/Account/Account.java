af57d10099cc041d4ad10a12f7e33c3decbdd0a8
hayk-bvb <hayk-bvb@github.com>
2021-11-27 21:35:38 -0500

    Added Javadocs

--- src/main/java/Account/Account.java
+++ src/main/java/Account/Account.java
@@ -51,25 +51,34 @@ public class Account implements Serializable {
         return this.masterPassword;
     }
 
+
     /**
-     * A getter method to get a copy of this account's vault
-     *
-     * @return Returns a copy of this account's vault, which is an instance of PrivateInfoManager
+     * A getter method to get this account's vault which is an ArrayList of PrivateInfo.
+     * @return Returns the vault which is an ArrayList of PrivateInfo.
      */
-//    public PrivateInfoManager getVault() {
-//        return this.vault;
-//    }
     public ArrayList<PrivateInfo> getVault() {
         return this.vault;
     }
 
 
+    /**
+     * This method is responsible for adding new instances of PrivateInfo into the vault of this account.
+     * @param newInfo The new PrivateInfo that is to be added to the vault.
+     */
     public void addInfo(PrivateInfo newInfo) {
 
         this.vault.add(newInfo);
 
     }
 
+    /**
+     * This method is responsible for editing the wanted instance of PrivateInfo from the vault.
+     *
+     * @param infoId The unique UUID of the instance of PrivateInfo.
+     * @param attributeToChange The string representation of the wanted attribute to change, such as "username" or "password".
+     * @param newValue The new string value that is to be changed to.
+     * @throws Throwable Throws Exception.
+     */
     public void editInfo(UUID infoId, String attributeToChange, String newValue) throws Throwable {
 
         for (PrivateInfo info : this.vault) {
@@ -85,6 +94,11 @@ public class Account implements Serializable {
 
     }
 
+    /**
+     * This method is responsible for deleting the wanted instance of PrivateInfo from the vault.
+     *
+     * @param infoId The unique UUID for the instance of PrivateInfo that is wanted to be deleted.
+     */
     public void deleteInfo(UUID infoId) {
 
         int i = 0;
@@ -101,6 +115,13 @@ public class Account implements Serializable {
     }
 
 
+    /**
+     * This is a getter method responsible for getting the wanted PrivateInfo from the vault.
+     *
+     * @param infoId The unique UUID of the wanted PrivateInfo instance.
+     * @return Returns the wanted PrivateInfo instance.
+     * @throws Throwable Throws Exception.
+     */
     public PrivateInfo getPrivateInfo(UUID infoId) throws Throwable{
 
         int i = 0;
